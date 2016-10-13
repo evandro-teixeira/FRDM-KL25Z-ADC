4b233a2d91074e3c2afec970b2cc0c11398782d1
evandro-teixeira <evandrolst@live.com>
2016-10-12 23:52:06 -0300

    Biblioteca ADC
    Biblioteca ADC

---
+++ main.c
@@ -0,0 +1,36 @@
+/*
+Projeto: Aplicação com Biblioteca ADC 
+*/
+#include "MKL25Z4.h"
+#include "adc.h"
+#include "gpio.h"
+
+uint32_t i = 0;
+uint16_t adc = 0;
+
+int main(void)
+{
+
+	adc_init(); // Inicializa ADC
+	gpio_direction(PORT_B,18,OUTPUT,NO_PULL_RESISTOR); // Inicializa IO PTB18 - LED Vermelho
+	gpio_direction(PORT_B,19,OUTPUT,NO_PULL_RESISTOR); // Inicializa IO PTB19 - LED Verde
+
+	gpio_set(PORT_B,19,1); // Desliga LED vermelho
+	gpio_set(PORT_B,18,1); // Desliga LED verde
+
+    for (;;)
+    {
+    	adc = adc_read( ADC_5 );	// Le ADC ~ PTB1
+
+    	if(adc > 50000)
+    	{
+    		gpio_set(PORT_B,19,0); // Liga LED vermelho
+    		gpio_set(PORT_B,18,1); // Desliga LED verde
+    	}
+    	else
+    	{
+    		gpio_set(PORT_B,18,0); // Liga LED verde
+    		gpio_set(PORT_B,19,1); // Desliga LED vermelho
+    	}
+	}
+}
